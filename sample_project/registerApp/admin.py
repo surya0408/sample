426b2018f1c82666111c4126da9b1ae64d5b1b60
surya0408 <58411965+surya0408@users.noreply.github.com>
2021-05-10 07:21:44 -0700

    Delete sample_project directory

--- sample_project/registerApp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from registerApp.models import UserModel
-# Register your models here.
-admin.site.register(UserModel)
--- sample_project/registerApp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from registerApp.models import UserModel
-
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    class Meta():
-        model = UserModel
-        fields = '__all__'
